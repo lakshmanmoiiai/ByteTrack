e602a36bb970f6f29ac310f62e82c738bd00c937
lakshman <lakshman@moiiai.com>
2022-10-16 13:26:30 +0200

    bug fix

--- tools/demo_track_deep_sort.py
+++ tools/demo_track_deep_sort.py
@@ -221,9 +221,8 @@ def imageflow_demo(predictor, vis_folder, current_time, args):
                     if tlwh[2] * tlwh[3] > args.min_box_area and not vertical:
                         online_tlwhs.append(tlwh)
                         online_ids.append(tid)
-                        online_scores.append(t.score)
                         results.append(
-                            f"{frame_id},{tid},{tlwh[0]:.2f},{tlwh[1]:.2f},{tlwh[2]:.2f},{tlwh[3]:.2f},{t.score:.2f},-1,-1,-1\n"
+                            f"{frame_id},{tid},{tlwh[0]:.2f},{tlwh[1]:.2f},{tlwh[2]:.2f},{tlwh[3]:.2f},-1,-1,-1\n"
                         )
                 timer.toc()
                 online_im = plot_tracking(
